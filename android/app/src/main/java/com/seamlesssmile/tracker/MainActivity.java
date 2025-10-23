ca8f69d5efcef2d673888dd6826ddafb68f91cec
necotur <najatee.kamal@gmail.com>
2025-10-24 01:06:35 +0300

    89

--- android/app/src/main/java/com/seamlesssmile/tracker/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.seamlesssmile.tracker;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- lib/i18n/index.ts
+++
@@ -1,6 +0,0 @@
-
-export { en } from './locales/en';
-export { ar } from './locales/ar';
-export { tr } from './locales/tr';
-export { ku } from './locales/ku';
-export type { Translations } from './locales/en';
